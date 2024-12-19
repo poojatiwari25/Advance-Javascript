26ed7622be65d18c4597711cb01700016ac2957e
Pooja Tiwari <poojatiwari2508@gmail.com>
2024-12-19 14:36:16 +0530

    Add files via upload

---
+++ JS_Advance/Inheritance/calc.js
@@ -0,0 +1,14 @@
+class Calculator{
+	add(a,b){
+		console.log('add is',a+b);
+	}
+	sub(a,b){
+		console.log('sub is',a-b);
+	}
+	mul(a,b){
+		console.log('mul is',a*b);
+	}
+	div(a,b){
+		console.log('div is',a/b);
+	}
+}
